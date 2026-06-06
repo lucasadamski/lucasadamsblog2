0ddb48507aa12c499fd0164066275c18393c3452
Lucas A <lucasadams@protonmail.com>
2026-06-06 18:29:04 +0100

    Implementing clickable menu on mobile

---
+++ scripts/scripts.js
@@ -0,0 +1,22 @@
+console.log("I'm in script.js file now.");
+let menuIsHidden = true;
+
+function onClickMenuList() {
+    console.log("onClickMenuList fired!");
+    let sideMenu = document.querySelector('.menu-list');
+    if(menuIsHidden === true) {
+        sideMenu.style.left = '0';
+        console.log("left set 0");
+        menuIsHidden = false;
+    }
+    else {
+        sideMenu.style.left = '-250px';    
+        console.log("left set -250");
+        menuIsHidden = true;
+    }
+}
+
+const menuLabel = document.querySelector('.menu-icon');
+menuLabel.addEventListener('click', () => {
+    onClickMenuList();
+});
